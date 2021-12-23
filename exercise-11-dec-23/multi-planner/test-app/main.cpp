9b420dd88e0d0be1b229fbe1114a2ee81e506c60
pavan.m <pavan.kumar@raibos.com>
2021-12-23 13:23:08 +0530

    converted to cpp file

--- exercise-11-dec-23/multi-planner/test-app/main.cpp
+++ exercise-11-dec-23/multi-planner/test-app/main.cpp
@@ -1,4 +1,6 @@
 
+#include <QCoreApplication>
+
 #include<stdio.h>
 #include<stdarg.h>
 #include<string.h>
@@ -11,13 +13,20 @@
 #include "linux/videodev2.h"
 #include "v4l-helpers.h"
 
+#include "opencv2/opencv.hpp"
+
 #define REQ_BUFF_SZ     5
+
 
 struct v4l_fd cam1;
 char cam_devpath[] = "/dev/video0";
 int realfd = 0;
-//open function verified 
-int open_device() 
+
+int IMAGE_W=0;
+int IMAGE_H=0;
+
+//open function verified
+int open_device()
 {
     int real_fd;
     v4l_fd_init( &cam1 );
@@ -52,11 +61,11 @@ int get_format()
     ret = v4l_g_fmt(&cam1,&vfmt,captype);
     unsigned pixelfmt = v4l_format_g_pixelformat( &vfmt );
     unsigned planes   = v4l_format_g_num_planes( &vfmt );
-    unsigned width  = v4l_format_g_width( &vfmt );
-    unsigned height = v4l_format_g_height( &vfmt );
+    IMAGE_W  = v4l_format_g_width( &vfmt );
+    IMAGE_H = v4l_format_g_height( &vfmt );
 
-    printf("width=%d\n",width);
-    printf("height=%d\n",height);
+    printf("width=%d\n",IMAGE_W);
+    printf("height=%d\n",IMAGE_H);
     printf("planes=%d\n",planes);
 
     printf("Video format: %c%c%c%c (%08x) %ux%u\n",
@@ -64,7 +73,7 @@ int get_format()
         (pixelfmt >> 8) & 0xff,
         (pixelfmt >> 16) & 0xff,
         (pixelfmt >> 24) & 0xff,
-        pixelfmt, width, height);
+        pixelfmt, IMAGE_W, IMAGE_H);
     return ret;
 }
 
@@ -78,21 +87,21 @@ int request_query_buffers()
 
     v4l_buffer_init( &vbuffer, captype, memtype, 0 );
     ret = v4l_buffer_prepare_buf( &cam1, &vbuffer );
-    if ( ret < 0 ) 
+    if ( ret < 0 )
         printf( "v4l_buffer_prepare_buf()" );
 
     v4l_queue_init( &vqueue, captype, memtype );
 
     ret = v4l_queue_querybufs( &cam1, &vqueue, 0 );
-    if ( ret < 0 ) 
+    if ( ret < 0 )
         printf( "v4l_queue_querybufs()" );
 
-    ret = v4l_queue_reqbufs( &cam1, &vqueue, REQ_BUFF_SZ, 0);
-    if ( ret < 0 ) 
+    ret = v4l_queue_reqbufs( &cam1, &vqueue, REQ_BUFF_SZ );
+    if ( ret < 0 )
         printf( "v4l_queue_reqbufs()" );
 
     ret = v4l_queue_mmap_bufs( &cam1, &vqueue, 0 );
-    if ( ret < 0 ) 
+    if ( ret < 0 )
         printf( "v4l_queue_mmap_bufs()" );
     return ret;
 }
@@ -102,7 +111,7 @@ int stream_on()
 {
     int type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
     int ret = v4l_ioctl( &cam1, VIDIOC_STREAMON, &type );
-    if ( ret < 0 ) 
+    if ( ret < 0 )
         printf( "v4l_ioctrl(VIDIOC_STREAMON)" );
     return  ret;
 }
@@ -112,10 +121,30 @@ int stream_off()
 {
     int type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
     int ret = v4l_ioctl(&cam1, VIDIOC_STREAMOFF, &type);
-    if ( ret < 0 ) 
+    if ( ret < 0 )
         printf( "v4l_ioctrl(VIDIOC_STREAMOFF)" );
     return ret;
 }
+
+cv::Mat BA12_to_RGB24(unsigned char *ba12)
+{
+    //https://answers.opencv.org/question/20748/convert-bayerbg12packed-12-bit-packed-into-rgb/
+    cv::Mat Mat16Bit(IMAGE_H, IMAGE_W, CV_16UC1, ba12);
+    cv::Mat Mat8Bit = Mat16Bit.clone();
+    Mat16Bit.convertTo(Mat8Bit, CV_8UC3, 1.0/256);
+    cv::Mat MatRgb(IMAGE_W, IMAGE_H, CV_8UC3);
+    //if (is_ov5648)
+    if (true)
+    {
+        cv::cvtColor(Mat8Bit, MatRgb, cv::COLOR_BayerBG2RGB);   //ov5648
+    }
+    else
+    {
+        cv::cvtColor(Mat8Bit, MatRgb, cv::COLOR_BayerGR2RGB);   //ar0144
+    }
+    return MatRgb;
+}
+
 
 int get_frame()
 {
@@ -132,11 +161,11 @@ int get_frame()
     tv.tv_usec = 0;
 
     ret = select( realfd + 1, &fds, NULL, NULL, &tv );
-    if ( ret <= 0 ) 
+    if ( ret <= 0 )
         printf( "wait sync failure.\n" );
 
     ret = v4l_buffer_dqbuf( &cam1, &vbuffer );
-    if ( ret < 0 ) 
+    if ( ret < 0 )
             printf( "v4l_buffer_dqbuf()" );
 
     unsigned bufflen = 0;
@@ -145,16 +174,20 @@ int get_frame()
     printf("bufflen = %u \n",bufflen);
     if ( bufflen > 0 )
     {
+        printf("ptrData > 0\n");
         size_t bidx = 0;
         void *ptrData = v4l_queue_g_mmapping( &vqueue, bidx, 0 );
         if ( ptrData != NULL )
         {
+            printf("ptrData != NULL\n");
+            cv::Mat mat = BA12_to_RGB24((unsigned char *)ptrData);
+            cv::imwrite("./sample.jpg",mat);
             // ptrData is RAW pixel array from camera or ISP.
         }
     }
 
     ret = v4l_buffer_qbuf( &cam1, &vbuffer );
-    if ( ret < 0 ) 
+    if ( ret < 0 )
         printf( "v4l_buffer_qbuf()" );
     return ret;
 }
@@ -164,8 +197,10 @@ int close_device()
     return v4l_close(&cam1);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    QCoreApplication a(argc, argv);
+
     int ret;
     ret = open_device();
     if(ret < 0) {
@@ -190,5 +225,6 @@ int main()
     //printf ("clear_buffers\n");
     close_device();
     printf ("close_device\n");
+    return 0;
+    return a.exec();
 }
-
